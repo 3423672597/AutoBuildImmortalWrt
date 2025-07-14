df5de85e9c31dc60392ab5e4e83346a91892b3fb
DingZW <142407191+3423672597@users.noreply.github.com>
2025-07-14 13:58:09 +0800

    Update build.sh

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -50,6 +50,28 @@ PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
+
+# ====自添加====
+#PACKAGES="$PACKAGES "
+#格式如上
+PACKAGES="$PACKAGES luci-app-autoreboot"
+PACKAGES="$PACKAGES luci-i18n-ddns-go-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-eqos-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-ipsec-vpnd-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-mwan3-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-openlist-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-openvpn-server-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-qos-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-ramfree-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-softethervpn-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-upnp-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-usb-printer-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-zerotier-zh-cn"
+
+
+
+# ====结束自添加====
+
 # 静态文件服务器dufs(推荐)
 PACKAGES="$PACKAGES luci-i18n-dufs-zh-cn"
 
@@ -62,7 +84,7 @@ PACKAGES="$PACKAGES luci-i18n-dufs-zh-cn"
 # 去广告adghome
 #PACKAGES="$PACKAGES luci-app-adguardhome"
 # 代理相关
-#PACKAGES="$PACKAGES luci-app-ssr-plus"
+PACKAGES="$PACKAGES luci-app-ssr-plus"
 #PACKAGES="$PACKAGES luci-app-passwall2"
 #PACKAGES="$PACKAGES luci-i18n-nikki-zh-cn"
 # VPN
